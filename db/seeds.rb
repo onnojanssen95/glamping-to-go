fbade44b069c5aebbe10f2dc31bca3be85ae50c5
ion iapara <iapara.ion.g@gmail.com>
2022-05-24 16:02:02 +0200

    seeeeeeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 8)
+  )
+  user.save!
+  10.times do
+    glamping_set = GlampingSet.new(
+      name: Faker::FunnyName.name,
+      description: Faker::Lorem.paragraph,
+      total_occupancy: rand(1..10),
+      price_per_night: rand(200..2000),
+      photo_url: "https://source.unsplash.com/random"
+    )
+    glamping_set.user = user
+    glamping_set.save!
+  end
+end
+
+# 10.times do
+#   bookings = Booking.create(
+#     start_date: Faker::Date.between(from: '2022-03-01', to: '2022-05-25'),
+#     end_date: Faker::Date.between(from: '2022-03-01', to: '2022-05-25'),
+#     price_per_night: Faker::Number.between(from: 100, to: 2000),
+#     set_id:
+#     guest_id:
+#     pickup_type: ["delivery", "pick-up", "delivery plus installation",].sample
+#   )
+# end
